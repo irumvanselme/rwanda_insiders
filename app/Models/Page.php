4b1be7c145e754a2f46952eb49f5198bcdacf4d2
irumvanselme <andesanselme@gmail.com>
2021-08-14 10:41:24 +0200

    Create the Page Class

---
+++ app/Models/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\File;
+
+class Page
+{
+    public $filename;
+
+    public function __construct()
+    {
+        $this->filename = collect(File::allFiles(app_path('pages/Posts')))
+            ->sortByDesc(function ($file) {
+                return $file->getBaseName();
+            })->map(function ($file) {
+                return $file->getBaseName();
+            });
+    }
+}
